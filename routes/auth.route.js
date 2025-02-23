49dce2c6d16007410a645000e27416bd5b17f1fb
kamilla <puffy.eyes@mail.ru>
2025-02-23 13:23:27 +0300

    Валидация v.1

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -8,6 +8,17 @@ router.post(
       try {
           const { email, password } = req.body;
 
+          // Валидация email
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          if (!emailRegex.test(email)) {
+              return res.status(400).json({ message: 'Некорректный email. Убедитесь, что email содержит символ "@" и имеет правильный формат.' });
+          }
+
+          // Валидация пароля
+          if (password.length < 6) {
+              return res.status(400).json({ message: 'Пароль должен содержать не менее 6 символов' });
+          }
+
           const isUsed = await User.findOne({ email });
 
           if (isUsed) {
@@ -24,7 +35,7 @@ router.post(
 
       } catch (error) {
           console.log(error);
-          res.status(500).json({ message: 'Ошибка сервера' }); // добавлено: обработка ошибок
+          res.status(500).json({ message: 'Ошибка сервера' }); 
       }
   }
 );
