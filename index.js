9bab34667ce7804e7a5f170e19b7c555007a1cae
kamilla <puffy.eyes@mail.ru>
2025-02-23 11:39:27 +0300

    Базовая регистрация

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// import process from 'process';
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/auth', require('./routes/auth.route'))
-
+app.use(cors()); 
+app.use(express.json({ extended: true }));
+app.use('/api/auth', require('./routes/auth.route'));
 
 async function start() {
   try {
